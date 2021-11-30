d21e5bf6655b61e021cae11d633e555a151ba1fb
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-11-30 14:02:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
+char conversione(char carattere){
+    if((carattere >= 'a'&& carattere <='z') || (carattere >='A' && carattere <='Z')){
+        if(carattere >= 'a' && carattere <= 'z'){
+            carattere = carattere-32;
+            return carattere;
+        }else if(carattere>= 'A' && carattere <= 'Z'){
+            carattere = carattere+32;
+            return carattere;
+        }else{
+            return '0';
+        }
+    }
 }
