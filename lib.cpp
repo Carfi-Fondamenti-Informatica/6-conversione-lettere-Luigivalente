87fe86875c0ccc8bc4a55e2cf0eb56d9b0ca4ee1
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-12-01 10:00:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+char conversione(char carattere){
+    if((carattere >= 'a'&& carattere <='z') || (carattere >='A' && carattere <='Z')){
+        if(carattere >= 'a' && carattere <= 'z'){
+            carattere = carattere-32;
+            return carattere;
+        }else if(carattere>= 'A' && carattere <= 'Z'){
+            carattere = carattere+32;
+            return carattere;
+        }else{
+            return '0';
+        }
+   
